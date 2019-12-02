7cfb1685d694d38df6a1f925b40529e0f11bffc3
Nate Tucker <ntuckertriplet@gmail.com>
2019-12-01 22:39:23 -0600

    day 1

---
+++ day1/day1.py
@@ -0,0 +1,26 @@
+import math
+
+
+def calculate(mass):
+    return math.floor(float(mass) / 3.0) - 2
+
+
+def recurse(int_input):
+    final = int_input // 3 - 2
+    if final <= 0:
+        return 0
+    return final + recurse(final)
+
+
+final_answer_part_1 = 0
+final_answer_part_2 = 0
+
+with open("data.txt", "r") as file:
+    data = file.readlines()
+
+    for line in data:
+        final_answer_part_1 += calculate(int(line))
+        final_answer_part_2 += recurse(int(line))
+
+    print(final_answer_part_1)
+    print(final_answer_part_2)
